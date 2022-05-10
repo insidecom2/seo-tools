0f6e6450c9d8379f76ce0dd60afaefef8472df71
jakkrit <jakkritt26@gmail.com>
2022-05-10 09:13:07 +0700

    update to Typescript

---
+++ pages/index.tsx
@@ -0,0 +1,20 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+import { useState } from 'react';
+
+export default function Home() {
+  const [show, setShow] = useState(true);
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Login In</title>
+        <meta name="description" content="Login" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+   
+    </div>
+  )
+}
