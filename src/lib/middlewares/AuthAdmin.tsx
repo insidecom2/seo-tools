5a02a4a56aa86dc1abf9f285815440af6538d6c8
jakkrit <jakkritt26@gmail.com>
2022-05-24 12:04:55 +0700

    auth middleware change

--- src/lib/middlewares/AuthAdmin.tsx
+++ src/lib/middlewares/AuthAdmin.tsx
@@ -3,19 +3,18 @@
 import { NextApiRequest, NextApiResponse } from "next";
 const jwt = require('jsonwebtoken');
 
-const AuthAdmin = async (req: NextApiRequest, res: NextApiResponse)=>{
+const AuthAdmin = async (req: NextApiRequest)=>{
     const header = req.headers;
    
-    if (!header.authorization) {
-         res.status(401).send(401);
+    if (!header?.authorization) {
+        return false;
     }
     try {
         const token = header.authorization.replace("Bearer ", "");
         return await jwt.verify(token, process.env.NEXT_JWT_SECERT);
     } catch (error) {
-        res.status(401).send(401);
+        return false;
     }
-        res.status(401).send(401);
 }
 
 export default AuthAdmin;
