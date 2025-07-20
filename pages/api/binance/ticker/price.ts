02cda7e8e71c14b5e8c7adb223b6567a9e02e49e
krit.pra <krit.prasert1983@gmail.com>
2025-07-20 11:17:18 +0700

    add call binance api get price

---
+++ pages/api/binance/ticker/price.ts
@@ -0,0 +1,29 @@
+import { NextApiRequest, NextApiResponse } from "next/types";
+import axios from "axios";
+import { timeStamp } from "console";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  switch (req.method) {
+    case "GET":
+      const { symbol } = req.query;
+      const data: any = await getPriceBinance(symbol as string); // BTCUSDT
+      const nowISO = new Date().toISOString();
+      return res.status(200).json({ price: data.price, timeStamp: nowISO });
+    default:
+      return res.status(405).end(`Method ${req.method} Not Allowed`);
+  }
+}
+
+const getPriceBinance = (symbol: string) => {
+  return axios
+    .get(`https://api.binance.com/api/v3/ticker/price?symbol=${symbol}`)
+    .then((response) => {
+      return response.data;
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+    });
+};
