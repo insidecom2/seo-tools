efd1fcfe6c5c9715ac8020a832b3b756cabe04cf
krit.pra <krit.prasert1983@gmail.com>
2025-11-15 17:03:20 +0700

    feat:add atr logic

--- src/components/binance/future_log/index.tsx
+++ src/components/binance/future_log/index.tsx
@@ -6,7 +6,7 @@ import {
 } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useEffect, useState } from "react";
-import { Button, Col, Pagination, Row, Table } from "react-bootstrap";
+import { Button, Col, Row, Table } from "react-bootstrap";
 import { LoadingIcon } from "../../common/loading";
 import useFutureLogs from "./hook/useLogs";
 
@@ -69,9 +69,9 @@ export const FutureLogsComm = () => {
           </Button>
         </Col>
       </Row>
-      <Table bordered hover responsive>
+      <Table bordered hover responsive style={{ fontSize: "14px" }}>
         <thead>
-          <tr>
+          <tr style={{ fontSize: "14px" }}>
             <th>#</th>
             <th>Symbol</th>
             <th>DateTime</th>
@@ -85,6 +85,7 @@ export const FutureLogsComm = () => {
             <th>CURRENT PRICE</th>
             <th>VOLUME LAST 20</th>
             <th>VOLUME LASTED</th>
+            <th>ATR</th>
             <th>SIGNAL</th>
           </tr>
         </thead>
@@ -92,6 +93,7 @@ export const FutureLogsComm = () => {
           {lists &&
             lists.map((row, index) => {
               const body = JSON.parse(row.body_json);
+              console.log(body);
               const bgOrder =
                 body.signal == "BUY" || body.signal == "SELL"
                   ? "bg-success text-white"
@@ -116,26 +118,33 @@ export const FutureLogsComm = () => {
                   <td>{body.current_price.toFixed(6)}</td>
                   <td>{body?.volume_last_20?.toFixed(4)}</td>
                   <td>{body?.volume_lasted?.toFixed(4)}</td>
+                  <td>{body?.atr_oder ? "Yes" : "No"}</td>
                   <td>{body.signal}</td>
                 </tr>
               );
             })}
         </tbody>
       </Table>
-
-      <div className="d-flex justify-content-end">
-        <Pagination>
-          {Array.from({ length: pagination?.page_all }, (_, i) => (
-            <Pagination.Item
-              key={i}
-              active={i + 1 === pagination?.page}
-              onClick={() => updatePage(i + 1)}
-            >
-              {i + 1}
-            </Pagination.Item>
-          ))}
-        </Pagination>
-      </div>
+      <Row className="pb-3">
+        <Col xs={1}>
+          <Button onClick={() => changePageStep("left")}>
+            <FontAwesomeIcon icon={faChevronLeft} />
+          </Button>
+        </Col>
+        <Col xs={10}>
+          <div className="px-2 d-flex gap-2 ">
+            <h2>
+              Future Logs ({paginationTable.page}/{pagination?.page_all})
+            </h2>
+            <h2>Total : {DecimalFormat(pagination?.all, 0)}</h2>
+          </div>
+        </Col>
+        <Col xs={1} className="text-end">
+          <Button onClick={() => changePageStep("right")}>
+            <FontAwesomeIcon icon={faChevronRight} />
+          </Button>
+        </Col>
+      </Row>
     </div>
   );
 };
